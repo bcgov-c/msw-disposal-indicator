acaa9394891716cca0f9ec455dfa81575c5983cb
Seb Dalgarno <seb@poissonconsulting.ca>
2018-11-03 16:55:35 -0700

    change hex var names, fortify sf

--- dataviz/app/global.R
+++ dataviz/app/global.R
@@ -4,12 +4,14 @@ library(ggiraph)
 library(patchwork)
 library(forcats)
 
+
 source("server_functions.R", local = TRUE)
 
 # read data
 indicator <- readRDS("data/indicator.rds") 
 indicator_summary <- readRDS("data/indicator_summary.rds") 
 district <- readRDS("data/district.rds")
+district_fort <- readRDS("data/district_fort.rds")
 coastline <- readRDS("data/coastline.rds")
 link <- readRDS("data/link.rds")
 
@@ -26,13 +28,13 @@ tooltip_css <- "background: white; opacity: 1; color: black; border-radius: 5px;
                 padding: 5px; box-shadow: 3px 3px 5px 0px #888888;
                 font-size: 12px; border-width 2px; border-color: black;"
 
-msw_blue <- "#808080"
-msw_green <- "#6ac1a5"
-msw_orange <- "#fa8d67"
-msw_hover <- "#808080"
-msw_select <- "#808080"
-msw_grey <- "#909090"
-msw_na <- "white"
+hex_bar2 <- "#808080"
+hex_bar1_1 <- "#6ac1a5"
+hex_bar1_2 <- "#fa8d67"
+hex_hover <- "#808080"
+hex_select <- "#808080"
+hex_axis <- "#909090"
+hex_na <- "white"
 
 tints <- rev(c("#485165", "#566179", "#64718D", "#7281A1", "#8091B5", "#8EA1C9", "#99AACE", "#A4B3D3", "#AFBCD8", "#BAC5DD", "#C5CEE2", "#D0D7E7", "#DBE0EC", "#E6E9F1", "#F1F2F6"))
 
